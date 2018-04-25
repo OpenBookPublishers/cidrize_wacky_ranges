4fef827a0b910a7ea49406d5ccff6610c554d8d9
Chuan Tan <ct538@cam.ac.uk>
2018-04-25 14:33:16 +0100

    Add function to handle excel file

--- json_outputter.py
+++ json_outputter.py
@@ -17,5 +17,13 @@
 #       "Contact"     : "a123@cam.ac.uk"
 #       "IP-Range"    : ["123.332.23.2/24","223.225.110.23/32"]
 #       }
+import sys
+from openpyxl import load_workbook
+import standardise_ip
 
+def run():
+    _, inputf, sheetname, institution_col_id , country_col_id, contact_col_id, ip_col_id = sys.argv
+    wb = load_workbook(inputf)
+    sheet = wb[sheetname]
+    row_ids_ip_range = standardise_ip.process(sheet,int(ip_col_id))
     
